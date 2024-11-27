a52a691e73f7b5817cac6f2cda76a1f8a03e3ea6
Puttipong Srisaard <bank0956581633c@gmail.com>
2024-11-27 22:09:03 +0700

    แก้ไขหน้าhome

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { SnowflakesComponent } from '../snowflakes/snowflakes.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SnowflakesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
