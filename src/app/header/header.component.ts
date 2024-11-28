1c573432fb40cabf61520ed788f53b975618df6b
Puttipong Srisaard <bank0956581633c@gmail.com>
2024-11-28 18:39:29 +0700

    แก้ swal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,17 +13,16 @@ export class HeaderComponent {
   
   logout() {
     Swal.fire({
-      title: 'Are you sure?',
-      text: 'You will be logged out!',
+      title: 'ต้องการออกจากระบบใช่หรือไม่',
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonText: 'Yes, Logout!',
-      cancelButtonText: 'Cancel'
+      confirmButtonText: 'ออกจากระบบ',
+      cancelButtonText: 'ยกเลิก'
     }).then((result) => {
       if (result.isConfirmed) {
         localStorage.removeItem('token');
         this.router.navigate(['/']);
-        Swal.fire('Logged out!', 'You have been logged out.', 'success');
+        // Swal.fire('Logged out!', 'You have been logged out.', 'success');
       }
     });
   }
